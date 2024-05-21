15c4dbc5b9c993a124db36ed4178e478c99de222
moscowmule7 <oguzsamsa@gmail.com>
2024-05-22 01:09:58 +0300

    Initial commit

---
+++ src/main/java/com/workintech/samsawear/SamsawearApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.samsawear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsawearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamsawearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/samsawear/SamsawearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.samsawear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsawearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
